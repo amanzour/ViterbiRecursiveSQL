e2fba8f374137c62f7a9dce5a52cbef6057cc5bd
Amir Manzour <amirmanzour@Amirs-MacBook-Air.local>
2022-08-07 23:01:00 -0400

    comenting SQL

--- recursiveSQL.sql
+++ recursiveSQL.sql
@@ -1,10 +1,10 @@
-# PostgreSQL 14
+-- PostgreSQL 14
 
+-- initialization
 drop table if exists PI;
 create table PI(s_init int NOT NULL UNIQUE, w float);
 insert into PI values(1, 0.5); 
 insert into PI values(2, 0.5);
-select * from PI;
 
 drop table if exists A;
 create table A(s_source int, s_dest int, w float);
@@ -12,7 +12,6 @@ insert into A values(1, 1, 0.75);
 insert into A values(1, 2, 0.25);
 insert into A values(2, 1, 0.4);
 insert into A values(2, 2, 0.6);
-select * from A;
 
 drop table if exists B;
 create table B(s int, y int, prob float);
@@ -22,7 +21,6 @@ insert into B values(1, 3, 0.1);
 insert into B values(2, 1, 0.5);
 insert into B values(2, 2, 0.25);
 insert into B values(2, 3, 0.25);
-select * from B;
 
 drop table if exists Y;
 create table Y(j SERIAL, y int, j_next int GENERATED ALWAYS AS (j + 1) STORED);
@@ -32,23 +30,27 @@ insert into Y (y) values(3);
 insert into Y (y) values(1);
 insert into Y (y) values(3);
 insert into Y (y) values(2);
-select * from Y;
 
 
-
+-- optimization
 with recursive viterbi (j, s, x, likelihood) as
 (
 select Y.j, PI.s_init, array[0] as x, PI.w*B.prob as likelihood from PI 
     inner join B on PI.s_init=B.s 
     inner join Y on B.y=Y.y and Y.j = 1 
 union all
+ -- keeping only the columns we would need (read this third)
 select j, s_dest, x, likelihood from
-(select j, s_dest, x, likelihood, rank() over (partition by s_source order by likelihood desc) as ranking from (select Y.j, A.s_source, A.s_dest, x || array[A.s_source] as x, viterbi.likelihood*A.w*B.prob as likelihood from A
+ -- keeping only records with most highest likelihood (read this second)   
+(select j, s_dest, x, likelihood, rank() over (partition by s_source order by likelihood desc) as ranking from 
+ -- the actual recursive call (read this first)
+ (select Y.j, A.s_source, A.s_dest, x || array[A.s_source] as x, viterbi.likelihood*A.w*B.prob as likelihood from A
     inner join viterbi on A.s_source = viterbi.s
     inner join B on A.s_dest=B.s 
     inner join Y on B.y=Y.y and viterbi.j + 1 = Y.j
     ) as t) as d where d.ranking = 1
 )
-select j, s, x || array[s] as states, likelihood from viterbi
+-- selecting the most likely path and its corresponding final state
+select j, s, x[2:cardinality(x)] || array[s] as states, likelihood from viterbi
 where j in (select max(j) from viterbi)
 order by likelihood desc limit 1;
